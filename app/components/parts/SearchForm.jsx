e3f624c4ec1765ed52f12f6d9aa45f1442147a9d
duncanleung <leungd@gmail.com>
2016-01-05 16:24:38 -0800

    (#1) FixBug - Use React Compatible autoFocus for Search Bar

--- app/components/parts/SearchForm.jsx
+++ app/components/parts/SearchForm.jsx
@@ -15,7 +15,7 @@ var SearchForm = React.createClass({
       <div id="search-bar">
           <form className="input-group" action="javascript:void(0)" onSubmit={ this.search }>
             <input id="search" ref="keyword" type="search" placeholder="Enter Brand or Keyword"
-                    autofocus="autofocus" className="form-control" />
+                    autoFocus="autofocus" className="form-control" />
             <span className="input-group-btn">
               <button id="submit" type="button" className="btn btn-default">Search</button>
             </span>
